9614cfe005c148ad256c310bb0168797b7934f47
Crew Spence <csspence123@gmail.com>
2020-12-30 08:16:23 -0600

    Wrote working solution.

--- codewars_usd_to_cny.js
+++ codewars_usd_to_cny.js
@@ -8,6 +8,21 @@ For Example:
 The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers shold be rounded to the nearest hundredth (e.g. 21.00 NOT 21.0 or 21) (HINT: google toFixed)
 */
 
-function usdcny(usd) {
-  
+const usdcny = (usd) => {
+  let cny = usd * 6.75;
+  let str = cny.toString();
+  let arr = str.split('.')
+  if(arr.length === 1) {
+    return arr[0] + '.00 Chinese Yuan';
+  }
+  if(arr[1].length === 2) {
+    return arr[0] + '.' + arr[1] + ' Chinese Yuan';
+  } else if(arr[1].length > 2) {
+    if(arr[1][2] >= 5) {
+      arr[1][1] = Number(arr[1][1]) + 1;
+    }
+    return arr[0] + '.' + arr[1] + ' Chinese Yuan';
+  } else {
+    return arr[0] + '.' + arr[1] + '0 Chinese Yuan';
+  }
 }
